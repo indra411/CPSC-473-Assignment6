0f71cedd1f68da80c05b27f1c94b087f7919ebcd
Indrawan Saputra <indrawan@csu.fullerton.edu>
2016-04-11 13:16:10 -0700

    jshint Please enter the commit message for your changes. Lines starting

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ Indrawan Saputra (indrawan@csu.fullerton.edu)
 CPSC 473-01 Assignment 6
 4/11/2016
 */
+var require, response;
 var express = require("express"),
-	http = require("http"),
-	app;
+    http = require("http"),
+    app;
 
 var bodyParser = require("body-parser");
 
 var urlencodedParser = bodyParser.urlencoded({
-	extended: false
-})
+    extended: false
+});
 
 // Express powered HTTP Server
 app = express();
@@ -21,15 +22,15 @@ app.use(express.static("public"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 
 var redis = require("redis"),
-	client = redis.createClient(),
-	$coinFlips = {} ;
+    client = redis.createClient(),
+    $coinFlips = {} ;
 
-	$coinFlips.wins = 0;
-	$coinFlips.losses = 0;
+    $coinFlips.wins = 0;
+    $coinFlips.losses = 0;
 
 app.get("/hello", function(req, res) {
     res.send("hello world!");
@@ -42,9 +43,9 @@ app.post("/flip", urlencodedParser, function(req, res) {
     var randomNum = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
     console.log(randomNum);
     console.log(req.body);
-    console.log(req.body["call"]);
+    console.log(req.body.call);
 
-    if (req.body["call"] == coin[randomNum]) {
+    if (req.body.call == coin[randomNum]) {
         client.incr("wins"); //incrementing wins
 
         console.log("Matched - you win");
